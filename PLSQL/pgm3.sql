a6b9086cad02f3ceb6100726bf38a4899c469629
Anandu kk <116473698+kkanandu@users.noreply.github.com>
2023-07-13 12:19:53 +0530

    Add files via upload

---
+++ PLSQL/pgm3.sql
@@ -0,0 +1,13 @@
+   use db1;
+ 
+DELIMITER //
+ CREATE PROCEDURE `SP_Sum`(IN `n1` INT,IN `n2` INT,OUT `result` INT)
+ BEGIN
+ Set result = n1+n2;
+ END //
+ DELIMITER ;
+ 
+ 
+ Call SP_Sum(2,3,@result);
+ SELECT @result;
+ 
